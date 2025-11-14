32d44b718fdd2b648761bdfcb19e78815d2e5178
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-11-14 18:01:20 -0300

    feature: Agregar estructura de proyecto inicial para backend y proxy

---
+++ backend/src/main/java/com/juanalejop/backend/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data transfer objects mappers.
+ */
+package com.juanalejop.backend.service.mapper;
---
+++ backend/src/main/java/com/juanalejop/backend/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.juanalejop.backend.web.rest.errors;
